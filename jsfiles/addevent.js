706782cc5799bdd18bc430962c248b5f4a85c2f1
Rohan Puranik <59736919+Rohan-3@users.noreply.github.com>
2023-10-15 19:44:02 +0530

    Practise

---
+++ jsfiles/addevent.js
@@ -0,0 +1,7 @@
+document.getElementById("btn").addEventListener("click",DisplayDate )
+
+
+function DisplayDate(){
+    alert("hello")   
+        document.getElementById("dem").innerHTML=Date()
+    }
