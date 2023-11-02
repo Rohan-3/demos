b9f681a01b0ce7022f06848be7fb28af21aebdf6
Rohan Puranik <59736919+Rohan-3@users.noreply.github.com>
2023-11-02 13:28:04 +0530

    Prac

---
+++ jsfiles/wel2.js
@@ -0,0 +1,4 @@
+var name1 = new String("user")
+var name2 = new String("user")
+
+console.log(name1 === name2);
---
+++ jsfiles/welcomeJS.js
@@ -0,0 +1,10 @@
+function myfun() {
+    document.getElementById("show").innerHTML="What now?"
+}
+
+function num(p1,p2) {
+    
+    return p1+p2
+}
+console.log(num(99,1));
+document.getElementById("show").innerHTML=alert(3+4)
